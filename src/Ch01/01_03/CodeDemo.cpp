531788bf0e3b7325a5f8898476f3527a7f238e97
VaverkaFilip <113186336+VaverkaFilip@users.noreply.github.com>
2026-07-16 22:49:49 +0200

    Update CodeDemo.cpp

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -6,9 +6,13 @@
 #include <string>
 
 int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
+    std::string str_fn;
+    std::string str_ln;
+    std::cout << "Enter your first name: "<< std::endl;
+    std::cin >> str_fn;
+    std::cout << "Enter your last name: "<< std::endl;
+    std::cin >> str_ln;
+    std::cout << "Your name is: " << str_fn <<" "<<str_ln<< std::sndl;
 
     std::cout << std::endl << std::endl;
     return (0);
